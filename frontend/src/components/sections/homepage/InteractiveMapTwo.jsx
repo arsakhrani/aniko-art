18ef402e61930b5fb937da7d9c6043d34f09ce5f
arsakhrani <arsakhrani@gmail.com>
2021-10-01 09:47:31 +0200

    interactive map completed with animations

--- frontend/src/components/sections/homepage/InteractiveMapTwo.jsx
+++ frontend/src/components/sections/homepage/InteractiveMapTwo.jsx
@@ -1,17 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { ReactComponent as Continent } from "../../../assets/icons/map/countries/continent.svg";
-
+import "./interactiveMap.css";
 
 export default function InteractiveMapTwo() {
-    return (
-        <div style={styles.container}>
-            <Continent/>
-        </div>
-    )
+  const [flag, setFlag] = useState(true);
+  const [allowAnimation, setAllowAnimation] = useState(true);
+
+  useEffect(() => {
+    const countries = document.getElementsByClassName("country");
+    const animated = document.getElementsByClassName("animated");
+    if (animated.length > 0) {
+      animated[0].classList.remove("animated");
+    }
+    if (allowAnimation) {
+      const randomIndex = Math.floor(Math.random() * (countries.length - 1));
+      countries[randomIndex].classList.add("animated");
+    }
+    setTimeout(() => {
+      setFlag(!flag);
+    }, 3000);
+  }, [flag]);
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.titleBox}>
+        <h1 style={{ fontSize: 44, marginBottom: 0 }}>Showroom</h1>
+        <p
+          style={{
+            color: "#F2A16B",
+            marginBottom: -10,
+            cursor: "pointer",
+            fontSize: 13,
+          }}
+        >
+          X VIEW ALL ARTWORKS
+        </p>
+        <div
+          style={{ height: 1, width: 135, backgroundColor: "#F2A16B" }}
+        ></div>
+      </div>
+      <Continent width="100vw" onMouseLeave={()=> setAllowAnimation(true)} onMouseEnter={() => setAllowAnimation(false)}/>
+    </div>
+  );
 }
 
 const styles = {
-    container: {
-      height: "100vh",
-    },
-}
+  container: {
+    height: "100vh",
+  },
+  titleBox: {
+    position: "absolute",
+    width: "35%",
+    height: "20%",
+    display: "flex",
+    flexDirection: "column",
+    paddingLeft: "6em",
+    justifyContent: "space-around",
+    fontFamily: "'Crimson Text', serif",
+  },
+};
