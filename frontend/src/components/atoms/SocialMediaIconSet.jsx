67f60eefc198cbfe5f6169af5dd9cebaec3077f2
arsakhrani <arsakhrani@gmail.com>
2022-01-29 16:58:51 +0100

    (feat) - WeChat popup added.

--- frontend/src/components/atoms/SocialMediaIconSet.jsx
+++ frontend/src/components/atoms/SocialMediaIconSet.jsx
@@ -1,11 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import { ReactComponent as Instagram } from "../../assets/icons/social-media/social-instagram-logo.svg"
 import { ReactComponent as Facebook } from "../../assets/icons/social-media/social-facebook-logo.svg"
 import { ReactComponent as Linkedin } from "../../assets/icons/social-media/social-linkedin-logo.svg"
 import { ReactComponent as Wechat } from "../../assets/icons/social-media/social-wechat-logo.svg"
-import { Container, Icon } from "./styles/SocialMediaIconSet.styled"
+import {
+  Container,
+  Icon,
+  QrContainer,
+} from "./styles/SocialMediaIconSet.styled"
+import weChatQrCode from "../../assets/images/we-chat.jpeg"
 
 export default function SocialMediaIconSet() {
+  const [showQrCode, setShowQrCode] = useState(false)
+
+  const vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  )
+
   return (
     <Container>
       <Icon>
@@ -26,9 +38,21 @@ export default function SocialMediaIconSet() {
           <Facebook />
         </a>
       </Icon>
-      <Icon>
-        <Wechat />
-      </Icon>
+      {vw > 600 && (
+        <Icon
+          onMouseEnter={() => setShowQrCode(true)}
+          onMouseLeave={() => {
+            setShowQrCode(false)
+          }}
+        >
+          <Wechat />
+        </Icon>
+      )}
+      {showQrCode && (
+        <QrContainer>
+          <img alt={"WeChat QR Code"} src={weChatQrCode} />
+        </QrContainer>
+      )}
     </Container>
   )
 }
--- frontend/src/components/atoms/styles/SocialMediaIconSet.styled.js
+++ frontend/src/components/atoms/styles/SocialMediaIconSet.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import theme from "../../common/theme"
 
 export const Container = styled.div`
   align-self: flex-end;
@@ -16,4 +17,20 @@ export const Icon = styled.div`
   align-items: center;
   border-radius: 10px;
   cursor: pointer;
+`
+
+export const QrContainer = styled.div`
+  position: absolute;
+  left: 5px;
+  bottom: 5px;
+  width: 15em;
+  height: 20em;
+  border: 3px solid ${theme.color.darkGrey};
+  border-radius: 5px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border: 10px solid ${theme.color.grey};
+  }
 `
